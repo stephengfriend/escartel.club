3df3613e0fa9887ce9092425a5d5df79b305e8f0
Stephen Friend <intrikit@hackinbook.nessie-vibe.ts.net>
2024-09-11 13:32:04 -0400

    Adds netlify, react, and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: netlify()
+});
